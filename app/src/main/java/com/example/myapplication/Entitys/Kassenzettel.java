c9bab9edef4dcb75871f66a78c2e3d569f2df0cd
RobinGscheidle <72752366+RobinGscheidle@users.noreply.github.com>
2022-05-16 20:14:02 +0200

    User und Kassenzettel

---
+++ app/src/main/java/com/example/myapplication/Entitys/Kassenzettel.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.Entitys;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import javax.annotation.Nonnull;
+
+@Entity(foreignKeys = {@ForeignKey(entity = User.class,
+        parentColumns = "kassenzettelId",
+        childColumns = "kassenzettelId",
+        onDelete = ForeignKey.CASCADE)}
+        ,tableName = "Kassenzettel")
+public class Kassenzettel {
+
+    @PrimaryKey(autoGenerate = true)
+    @Nonnull
+    @ColumnInfo(name = "kassenzettelId")
+    private int id;
+
+    @ColumnInfo(name = "userId")
+    private int userId;
+
+
+    public Kassenzettel(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+}
