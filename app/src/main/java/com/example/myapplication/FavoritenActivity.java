406e601c538d54009701e8d7a358802c23f287aa
ohva1011 <ohva1011@h-ka.de>
2022-06-11 22:48:38 +0200

    round home and filter button with icon

--- app/src/main/java/com/example/myapplication/FavoritenActivity.java
+++ app/src/main/java/com/example/myapplication/FavoritenActivity.java
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class FavoritenActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoriten);
+
     }
 
     /** Called when the user taps the Homescreen button */
     public void openHomescreen(View view) {
         Intent intent = new Intent(this, HomescreenActivity.class);
         startActivity(intent);
+
+
     }
 }
