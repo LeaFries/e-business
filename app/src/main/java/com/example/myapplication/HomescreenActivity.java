440b0f317bb70a44afcd022c92cc08e4672be9d2
eva-f00 <74498542+eva-f00@users.noreply.github.com>
2022-06-11 10:02:30 +0200

    Erste Versuche anLoad

--- app/src/main/java/com/example/myapplication/HomescreenActivity.java
+++ app/src/main/java/com/example/myapplication/HomescreenActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -11,12 +12,14 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.myapplication.Entitys.Hofautomat;
+import com.example.myapplication.Entitys.Produkt;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomescreenActivity  extends AppCompatActivity {
     URoomDatabase db;
+    private AppCompatButton loadButton;
     private TextView nameTextView;
     private ListView listView;
 
@@ -25,28 +28,59 @@ public class HomescreenActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
+        loadButton = findViewById(R.id.load_b);
+        nameTextView = findViewById(R.id.textView10);
+
+        //loadButton.setOnClickListener(this);
+
         db = URoomDatabase.getDatabase(this);
 
+    }
+
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.load_b:
+                onLoadButtonPressed();
+                break;
+
+        }
+
+    }
+
+    public void onSaveButtonPressed() {
+    }
+
+
+    /** Button zum Laden der Hofautomaten */
+    public void onLoadButtonPressed() {
         AsyncTask.execute(new Runnable() {
             @Override
             public void run() {
-
-               // final ArrayList<String> hofautomaten = db.hofautomatDAO().getHofautomatNames();
+                //Test: zuletzt gespeicherten Hofautomaten aus Datenbank holen
                 final Hofautomat hofautomat = db.hofautomatDAO().getLastHofautomat();
 
                 //Lade den Namen in die TextView
-               runOnUiThread(new Runnable() {
+                runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                       //nameTextView.setText(hofautomat.getName());
-                        //listView = (ListView)findViewById(R.id.listView);
-                       /* final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
-                                R.layout.activity_listview,
-                                hofautomaten);
-                        listView.setAdapter(arrayAdapter); */
+                        nameTextView.setText(hofautomat.getName());
                     }
                 });
             }
+            //Lade den Namen in die TextView
+           /** runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    //nameTextView.setText(hofautomat.getName());
+                    //listView = (ListView)findViewById(R.id.listView);
+                       /* final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                                R.layout.activity_listview,
+                                hofautomaten);
+                        listView.setAdapter(arrayAdapter);
+                }
+            });*/
         });
     }
 
